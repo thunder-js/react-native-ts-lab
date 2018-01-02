12e2d6f88957936cc07b4d22fc7e200b28671c91
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-02 16:29:37 -0200

    Simple snapshot testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View,
@@ -8,14 +7,8 @@ import {
 } from 'react-native';
 import codePush from 'react-native-code-push'
 import { Navigator } from 'detox'
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-class App extends React.Component<{}, { hello: boolean }> {
+export class App extends React.Component<{}, { hello: boolean }> {
   state = {
     hello: false
   }
--- src/modules/example/components/Example/Example.spec.tsx
+++ src/modules/example/components/Example/Example.spec.tsx
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Example from './';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <App />
-  );
+    <Example message="OI" />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });

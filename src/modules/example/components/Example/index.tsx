c229c2bbec2841d61d6c3c2460edfc2f55f583f4
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-28 19:14:19 -0200

    FlatList typescript example

--- src/modules/example/components/Example/index.tsx
+++ src/modules/example/components/Example/index.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+
+const myData = [{
+  name: 'Rafael',
+  age: 13,
+}]
+
+const MyFlatList = FlatList as FlatList<{ name: string, age: number}>
 
 const Example = ({
-  message
+  message,
 }) => (
   <View>
+    <MyFlatList
+      data={myData}
+      renderItem={(data) => (<Text>{data.item.name}</Text>)}
+    />
     <Text>Hello world !!!</Text>
     <Text>{message}</Text>
   </View>
